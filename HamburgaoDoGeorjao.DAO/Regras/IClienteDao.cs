6fd7bd16117a4074c727e7ac5d18c734ef2f18a9
ElderAndrade <elder_andrade@estudante.sesisenai.org.br>
2024-02-25 14:00:04 -0300

    atualização banco de dados e inicio de

---
+++ HamburgaoDoGeorjao.DAO/Regras/IClienteDao.cs
@@ -0,0 +1,14 @@
+﻿using HamburgaoDoGeorjao.DAO.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamburgaoDoGeorjao.DAO.Regras
+{
+    public interface IClienteDao : IDao<ClienteVo>
+    {
+
+    }
+}
